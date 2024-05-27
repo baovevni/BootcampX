8eddfce5888993d7b57bb1321b196179c75e04a7
Evgheni Barducov <52833790+baovevni@users.noreply.github.com>
2024-05-26 23:59:52 -0400

    Remove comments

--- migrations/students_cohorts.sql
+++ migrations/students_cohorts.sql
@@ -1,22 +1,3 @@
--- A cohort will have the following attributes:
-
--- id: A unique identifier
--- name: The name of the cohort
--- start_date: The cohorts' start date
--- end_date: The cohorts' end date
-
-
--- A student will have the following attributes:
-
--- id: A unique identifier
--- name: The full name of the student
--- email: The students' email address
--- phone: The students' phone number
--- github: The students' github profile url
--- start_date: The students' start date of the Bootcamp
--- end_date: The students' end date of the Bootcamp
--- cohort_id: The id of the cohort that the student is enrolled in
-
 CREATE TABLE cohorts (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
@@ -34,7 +15,3 @@ CREATE TABLE students (
   end_date DATE,
   cohort_id INTEGER REFERENCES cohorts(id) ON DELETE CASCADE
 );
-
-
-
--- https://discord.com/channels/687722968033525851/1244484449580875846
