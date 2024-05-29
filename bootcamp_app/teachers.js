d8cb047b6a1de754c995c0bbcd1bd7f9d9e16cbf
Evgheni Barducov <52833790+baovevni@users.noreply.github.com>
2024-05-29 16:48:08 -0400

    Fix parametrized queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,23 +8,25 @@ const pool = new Pool({
 });
 
 const cohortName = process.argv[2];
-
-pool
-  .query(
-    `
+const values = [cohortName];
+const queryString = `
 SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${cohortName}'
+WHERE cohorts.name = $1
 ORDER BY teacher;
 `
-  )
+pool
+  .query(queryString, values)
   .then((res) => {
     res.rows.forEach(row => {
       console.log(
         `${row.cohort}: ${row.teacher}`
       );
     });
+  })
+  .catch((err) => {
+    console.error('query error', err.stack);
   });
